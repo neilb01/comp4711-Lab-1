57084971e11d188bef3ce49c213cce0626738734
NB <nb23@live.ca>
2016-09-11 14:15:53 -0700

    Finished lab with comments added.

--- student.php
+++ student.php
@@ -1,13 +1,14 @@
 <?php
     /**
      * Created by PhpStorm.
-     * User: NB
+     * User: Neil Brar
      * Date: 2016-09-11
      * Time: 1:18 PM
      */
-
+     /* Student class to construct each student */
     class Student {
-
+        
+        /* Constructer to initialize student */
         function __construct()
         {
             $this->surname = '';
@@ -15,17 +16,22 @@
             $this->emails = array();
             $this->grades = array();
         }
-
+        
+        /* Function called to add email */
         function add_email($which, $address)
         {
             $this->emails[$which] = $address;
         }
 
+        /* Function called to add grades */
+
         function add_grade($grade)
         {
             $this->grades[] = $grade;
         }
-
+        
+        /* Function to calculate average grade */
+        
         function average()
         {
             $total = 0;
@@ -34,7 +40,7 @@
             return $total / count($this->grades);
         }
 
-
+        /* toString function used to display output */
         function toString()
         {
             $result = $this->first_name . ' ' . $this->surname;
